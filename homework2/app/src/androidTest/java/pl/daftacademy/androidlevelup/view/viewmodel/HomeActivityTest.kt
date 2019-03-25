b84452018725def34f462deeace8f331f102871e
jstarczewski <jan.a.starczewski@gmail.com>
2019-03-25 18:15:24 +0100

    Simple espresso tests added

--- homework2/app/src/androidTest/java/pl/daftacademy/androidlevelup/view/viewmodel/HomeActivityTest.kt
+++ homework2/app/src/androidTest/java/pl/daftacademy/androidlevelup/view/viewmodel/HomeActivityTest.kt
@@ -1,18 +1,23 @@
 package pl.daftacademy.androidlevelup.view.viewmodel
 
 import android.view.Gravity
+import android.view.MenuItem
+import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.DrawerMatchers.isClosed
 import androidx.test.espresso.contrib.DrawerMatchers.isOpen
-import androidx.test.espresso.matcher.ViewMatchers.withContentDescription
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.contrib.NavigationViewActions.navigateTo
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.rule.ActivityTestRule
+import junit.framework.Assert.assertEquals
+import org.hamcrest.CoreMatchers.allOf
+import org.hamcrest.CoreMatchers.instanceOf
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 import pl.daftacademy.androidlevelup.R
 import pl.daftacademy.androidlevelup.view.home.HomeActivity
 
@@ -23,14 +28,32 @@ class HomeActivityTest {
     var activityRule = ActivityTestRule(HomeActivity::class.java)
 
     @Test
-    fun clickedHomeIconOpensDrawer() {
-        onView(withId(R.id.drawer_layout)).check(matches(isClosed(Gravity.START)))
+    fun clickedHomeIconDrawerOpened() {
+        checkDrawerIsNotOpen()
         clickHomeToOpenDrawer()
         checkDrawerIsOpen()
     }
 
+    @Test
+    fun backPressButtonClickedTwiceWhenDrawerIsOpenedToCloseApp() {
+        clickHomeToOpenDrawer()
+        onView(isRoot()).perform(ViewActions.pressBack())
+        checkDrawerIsNotOpen()
+        assertEquals(activityRule.activity.isFinishing, false)
+        assertEquals(activityRule.activity.isDestroyed, false)
+    }
+
+    @Test
+    fun drawerItemSelectedThenToolbarTextChanged() {
+        clickHomeToOpenDrawer()
+        onView(withId(R.id.navigation_view)).perform(navigateTo(R.id.movies_action))
+        onView(allOf(instanceOf(TextView::class.java), withParent(withId(R.id.toolbar)))).check(matches(withText("Action")))
+    }
+
     private fun clickHomeToOpenDrawer() {
-        onView(withContentDescription(activityRule.activity.findViewById<Toolbar>(R.id.toolbar).navigationContentDescription as String)).perform(click())
+        onView(withContentDescription(activityRule.activity.findViewById<Toolbar>(R.id.toolbar).navigationContentDescription as String)).perform(
+            click()
+        )
     }
 
     private fun checkDrawerIsOpen() {
