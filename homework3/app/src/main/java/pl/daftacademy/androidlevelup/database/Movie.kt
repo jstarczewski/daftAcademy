b998dc2130f3d3c370b3e1db9923cd021e5d0725
jstarczewski <jan.a.starczewski@gmail.com>
2019-04-02 22:35:16 +0200

    No tests written, but compilation without warning

--- homework3/app/src/main/java/pl/daftacademy/androidlevelup/database/Movie.kt
+++ homework3/app/src/main/java/pl/daftacademy/androidlevelup/database/Movie.kt
@@ -3,35 +3,24 @@ package pl.daftacademy.androidlevelup.database
 import androidx.room.*
 
 @Entity(
+    indices = [Index(value = ["studioId"], name = "id_movie_studio")],
     foreignKeys = [
-        ForeignKey(entity = Studio::class, parentColumns = ["id"], childColumns = ["studioId"], onUpdate = ForeignKey.CASCADE)
+        ForeignKey(
+            entity = Studio::class,
+            parentColumns = ["id"],
+            childColumns = ["studioId"],
+            onUpdate = ForeignKey.CASCADE
+        )
     ]
 )
 class Movie(
     @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
     val id: Int,
     val title: String,
     val year: Int,
     val genres: String,
+    @ColumnInfo(name = "studioId")
     val studioId: Int
- //   @Embedded
-  //  @Ignore
-  //  var studio: Studio
 
-) /*{
-    fun toEntity() = pl.daftacademy.androidlevelup.entity.Movie(title, year, genres.split(','), "Studio")
-
-    companion object {
-        fun fromEntity(entity: Movie) =
-            pl.daftacademy.androidlevelup.database.Movie(0, entity.title, entity.year, entity.genres.joinToString(","), en)
-    }
-}*/
-/*
-class MovieAndStudio(
-    val id: Int,
-    val title: String,
-    val year: Int,
-    val genres: String,
-    @Relation(parentColumn = "id",)
-
-)*/
+)
--- homework3/app/src/main/java/pl/daftacademy/androidlevelup/database/Studio.kt
+++ homework3/app/src/main/java/pl/daftacademy/androidlevelup/database/Studio.kt
@@ -1,7 +1,13 @@
 package pl.daftacademy.androidlevelup.database
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity
-data class Studio(@PrimaryKey(autoGenerate = true) val id: Int, val name: String)
+data class Studio(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    val id: Int,
+    val name: String
+)
