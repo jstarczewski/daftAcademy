ef3a87ffa6e595dae9e7c5a66dbc23bb3748a086
jstarczewski <jan.a.starczewski@gmail.com>
2019-04-07 19:59:47 +0200

    Notification factory added

---
+++ homework4/app/src/main/java/com/clakestudio/pc/homework4/util/ext/SystemVersionVerificationExt.kt
@@ -0,0 +1,19 @@
+package com.clakestudio.pc.homework4.util.ext
+
+import android.os.Build
+
+fun fromAndroid(version: Int, block: () -> Unit) {
+    if(Build.VERSION.SDK_INT >= version) block()
+}
+
+fun toAndroid(version: Int, block: () -> Unit) {
+    if(Build.VERSION.SDK_INT <= version) block()
+}
+
+fun afterAndroid(version: Int, block: () -> Unit) {
+    if(Build.VERSION.SDK_INT > version) block()
+}
+
+fun beforeAndroid(version: Int, block: () -> Unit) {
+    if(Build.VERSION.SDK_INT < version) block()
+}
