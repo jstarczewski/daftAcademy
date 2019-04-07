8e62276a98edc1743d8097498b55688432290368
jstarczewski <jan.a.starczewski@gmail.com>
2019-04-07 20:48:58 +0200

    Boot reciever implemented but not working on huawei

--- homework4/app/src/main/java/com/clakestudio/pc/homework4/broadcast/BootReceiver.kt
+++ homework4/app/src/main/java/com/clakestudio/pc/homework4/broadcast/BootReceiver.kt
@@ -3,12 +3,30 @@ package com.clakestudio.pc.homework4.broadcast
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import com.clakestudio.pc.homework4.R
+import com.clakestudio.pc.homework4.util.NotificationFactory
+import com.clakestudio.pc.homework4.util.ext.fromAndroid
 
 class BootReceiver : BroadcastReceiver() {
 
+    private val notificationFactory by lazy { NotificationFactory() }
+
     override fun onReceive(context: Context?, intent: Intent?) {
+        when (intent?.action) {
 
-        
 
+            Intent.ACTION_BOOT_COMPLETED -> showBootNotification(context!!)
+        }
+    }
+
+    private fun showBootNotification(context: Context) {
+        fromAndroid(Build.VERSION_CODES.O) {
+            notificationFactory.createNotificationChannels(context)
+            notificationFactory.show(
+                context,
+                context.resources.getString(R.string.notification_boot_compleated_title),
+                context.resources.getString(R.string.notification_boot_compleated_message))
+        }
     }
 }
