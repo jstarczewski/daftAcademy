be6598c8bb47348b729d290e9888d0d740508be6
jstarczewski <jan.a.starczewski@gmail.com>
2019-04-07 21:31:36 +0200

    Alarm base implementation

--- homework4/app/src/main/java/com/clakestudio/pc/homework4/MainActivity.kt
+++ homework4/app/src/main/java/com/clakestudio/pc/homework4/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.clakestudio.pc.homework4
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.clakestudio.pc.homework4.broadcast.AlarmReceier
 
 class MainActivity : AppCompatActivity() {
+
+
+    private val alarmManager: AlarmManager
+        get() = getSystemService(Context.ALARM_SERVICE) as AlarmManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        schedulePeriodicAlarm()
+    }
+
+
+    private fun schedulePeriodicAlarm() {
+        Intent(this, AlarmReceier::class.java)
+            .apply { action = "com.clakestudio.pc.homework4.NOTIFY" }
+            .let { PendingIntent.getBroadcast(this, 0, it, 0) }
+            .let {
+                alarmManager.setRepeating(
+                    AlarmManager.RTC_WAKEUP,
+                    System.currentTimeMillis(),
+                    AlarmManager.INTERVAL_FIFTEEN_MINUTES,
+                    it)
+            }
     }
 }
--- homework4/app/src/main/java/com/clakestudio/pc/homework4/broadcast/AlarmReceier.kt
+++ homework4/app/src/main/java/com/clakestudio/pc/homework4/broadcast/AlarmReceier.kt
@@ -5,26 +5,26 @@ import android.content.Context
 import android.content.Intent
 import android.os.Build
 import com.clakestudio.pc.homework4.R
-import com.clakestudio.pc.homework4.util.NotificationFactory
 import com.clakestudio.pc.homework4.util.ext.fromAndroid
+import com.clakestudio.pc.homework4.util.NotificationFactory
 
-class BootReceiver : BroadcastReceiver() {
+class AlarmReceier : BroadcastReceiver(){
 
     private val notificationFactory by lazy { NotificationFactory() }
 
     override fun onReceive(context: Context?, intent: Intent?) {
-        when (intent?.action) {
-            Intent.ACTION_BOOT_COMPLETED -> showBootNotification(context!!)
+     when (intent?.action) {
+            Intent.ACTION_BOOT_COMPLETED -> showAlarmNotification(context!!)
         }
     }
 
-    private fun showBootNotification(context: Context) {
+    private fun showAlarmNotification(context: Context) {
         fromAndroid(Build.VERSION_CODES.O) {
             notificationFactory.createNotificationChannels(context)
             notificationFactory.show(
                 context,
-                context.resources.getString(R.string.notification_boot_compleated_title),
-                context.resources.getString(R.string.notification_boot_compleated_message))
+                context.resources.getString(R.string.notification_boot_alarm_title),
+                context.resources.getString(R.string.notification_boot_alarm_message))
         }
     }
 }
