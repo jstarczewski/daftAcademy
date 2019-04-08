463c6853f8ee2d04a1aec05e5c03ef7ea760e588
jstarczewski <jan.a.starczewski@gmail.com>
2019-04-08 11:04:21 +0200

    Foreground service with notification implemented

--- homework4/app/src/main/java/com/clakestudio/pc/homework4/MainActivity.kt
+++ homework4/app/src/main/java/com/clakestudio/pc/homework4/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.clakestudio.pc.homework4
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.clakestudio.pc.homework4.foreground.ScanningService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ContextCompat.startForegroundService(this, Intent(this, ScanningService::class.java))
+
+        btStopService.setOnClickListener {
+            stopService(Intent(this, ScanningService::class.java))
+        }
 
     }
 }
--- homework4/app/src/main/java/com/clakestudio/pc/homework4/foreground/ScanningService.kt
+++ homework4/app/src/main/java/com/clakestudio/pc/homework4/foreground/ScanningService.kt
@@ -3,39 +3,33 @@ package com.clakestudio.pc.homework4.foreground
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import android.widget.Toast
 import com.clakestudio.pc.homework4.util.NotificationFactory
 
 private const val NOTIFICATION_ID = 997
-private const val NOTIFICATION_TITLE = "Service Info"
-private const val NOTIFICATION_BODY = "Service running..."
+private const val NOTIFICATION_TITLE = "Skanowanie"
+private const val NOTIFICATION_BODY = "Skanowanie beconów aktywne..."
 
 class ScanningService : Service() {
 
-    private val TAG = "ScanningService"
     private val notificationFactory by lazy { NotificationFactory() }
-
-    override fun onBind(intent: Intent?): IBinder? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-
-    }
 
     override fun onCreate() {
         super.onCreate()
-
         startForeground(NOTIFICATION_ID, notificationFactory.create(this, NOTIFICATION_TITLE, NOTIFICATION_BODY))
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-    }
-
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        showStartsToast()
         return super.onStartCommand(intent, flags, startId)
     }
 
-    override fun onUnbind(intent: Intent?): Boolean {
-        return super.onUnbind(intent)
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+
     }
 
-
+    private fun showStartsToast() {
+        Toast.makeText(this, "Scanning... ", Toast.LENGTH_SHORT).show()
+    }
 }
